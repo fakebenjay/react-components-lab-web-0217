b8b0703c74d455143d76016c130266becc27f692
fakebenjay <fakebenjay@gmail.com>
2017-04-02 19:51:33 -0400

    Done.

--- index.js
+++ index.js
@@ -34,12 +34,10 @@ class ButcherShop extends React.Component{
   render() {
     return React.createElement('div', {className: 'butcher-shop'},
       React.createElement('p', {}, "Hello! We have the following products for sale today:"),
-      React.createElement('ul', {}, [
-        React.createElement('li', {}, "Tenderloin"),
-        React.createElement('li', {}, "Short ribs"),
-        React.createElement('li', {}, "Beef shin"),
-        React.createElement('li', {}, "Ribeye")
-      ])
+      React.createElement('ul', {}, BUTCHER_PRODUCTS.map((meat) => {
+        return React.createElement('li', {}, meat)
+        })
+      )
     )
   }
 }
